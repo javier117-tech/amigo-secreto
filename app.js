2ccf0199f3d2608a29e7d8bfaaa913ac390c35f7
unknown <maeztre1@gmail.com>
2025-02-02 11:46:10 -0500

    Capturar el valor del campo de entrada

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let lista = [];
-let ammigos = prompt('Escribe un nombre: ');
-function agregarNombres (){
-    lista.push(amigos);
-    console.log(lista);
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    console.log(nombreAmigo);
     return;
-}
-function validarEntrada(){
-
-}
-function visualizarLista(){
-
 } 
 function sorteoAleatorio() {
 
