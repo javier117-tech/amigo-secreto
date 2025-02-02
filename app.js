5a3cb4a7300e50ba4dbea6116804e6928b7638ee
unknown <maeztre1@gmail.com>
2025-02-02 13:38:16 -0500

    Actualización de la función agregarAmigo

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array que almacena los nombres de los amigos
 let amigos = [];
+ // Función para agregar un amigo
 function agregarAmigo() {
-    let nombreAmigo = document.getElementById('amigo').value;
+    // Capturar el valor del campo de texto
+    let nombreAmigo = document.getElementById('amigo').value.trim();
     console.log(nombreAmigo);
+    // validar si el campo esta vacío
     if (nombreAmigo === ''){
         alert("Error ingrese un nombre: ");
         return;
     } else {
-        amigos.push(nombreAmigo);
-        document.getElementById('nombreAmigo').value = "";
+        // Actualizar el array de amigos: 
+        amigos.push(nombreAmigo); 
+        //limpiar el campo de entrada
+        document.getElementById('amigo').value = "";
+        console.log(amigos);
+        mostrarListaAmigos();
     }
+}
+function mostrarListaAmigos(){
+    // Obtener el elemento <ul>
+    const listaAmigos = document.getElementById('listaAmigos');
+    // Limpiar la lista antes de actualizarla
+    listaAmigos.innerHTML = "";
+    // Recorrer el array y agregar cada nombre a la lista
+    amigos.forEach(function(friend){
+        //Crear un nuevo <li> para cada amigo
+        const li = document.createElement('li');
+        // Establecer el texto del <li> como el nombre del amigo
+        li.textContent = friend;
+        // Agregar el <li> a la lista <ul>
+        listaAmigos.appendChild(li);
+    });
 } 
 function sorteoAleatorio() {
 
