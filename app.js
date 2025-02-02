522b3e3db5a737806ecbda78cfdc4af2059944ef
unknown <maeztre1@gmail.com>
2025-02-02 10:35:01 -0500

    Agregar archivos de trabajo

---
+++ app.js
@@ -0,0 +1,17 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista = [];
+let ammigos = prompt('Escribe un nombre: ');
+function agregarNombres (){
+    lista.push(amigos);
+    console.log(lista);
+    return;
+}
+function validarEntrada(){
+
+}
+function visualizarLista(){
+
+} 
+function sorteoAleatorio() {
+
+}
