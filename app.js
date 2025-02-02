78ad320dbfd1170984cc45e7bce0daa9eec46210
unknown <maeztre1@gmail.com>
2025-02-02 12:59:20 -0500

    Actualizaciòn de la funciòn agregar amigos

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let lista = [];
+let amigos = [];
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
     console.log(nombreAmigo);
-    return;
+    if (nombreAmigo === ''){
+        alert("Error ingrese un nombre: ");
+        return;
+    } else {
+        amigos.push(nombreAmigo);
+        document.getElementById('nombreAmigo').value = "";
+    }
 } 
 function sorteoAleatorio() {
 
